5d801f1c521b6071d178076e85163457c59ff3d0
mohamedmohesn <82167985+mohamedmohesn@users.noreply.github.com>
2022-03-08 23:23:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set('view engine', 'ejs');
 // database connection
 const port = app.listen(process.env.PORT || 3000)
 
-const dbURI =process.env.dbURI;
+const dbURI = process.env.KEYURL;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true , useFindAndModify:false})
   .then((result) => console.log("3000"))
   .catch((err) => console.log(err));
